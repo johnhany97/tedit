38c4fff0ee14f7ef794ed74447ec46ac1d68012a
John Ayad <johnhany97@gmail.com>
2024-12-25 22:13:16 +0200

    disable & fix a few flags

--- tedit.c
+++ tedit.c
@@ -13,8 +13,15 @@ void enableRawMode() {
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  // turn off echoing (akin to sudo mode), and canonical mode (byte-by-byte, instead of line-by-line)
-  raw.c_lflag &= ~(ECHO | ICANON);
+  // fix ctrl-m, and turn off ctrl-s / ctrl-q
+  raw.c_iflag &= ~(ICRNL | IXON);
+  // turn off all output processing
+  raw.c_oflag &= ~(OPOST);
+  // turn off echoing (akin to sudo mode),
+  // and canonical mode (byte-by-byte, instead of line-by-line),
+  // and ctrl-v signal,
+  // and ctrl-c / ctrl-z signals
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -24,9 +31,9 @@ int main() {
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
       if (iscntrl(c)) {
-          printf("%d\n", c);
+          printf("%d\r\n", c);
       } else {
-          printf("%d ('%c')\n", c, c);
+          printf("%d ('%c')\r\n", c, c);
       }
   }
     
